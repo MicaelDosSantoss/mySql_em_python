df3d47466a4d5de54ebd06e4dd8ee54cccfe0be3
Micael Santos <micaelds243@gmail.com>
2023-03-10 20:09:02 -0300

    table

--- server.py
+++ server.py
@@ -13,12 +13,3 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE carros(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, carro VARCHAR(255) NOT NULL, placa VARCHAR(255) NOT NULL )")
 
 mycursor.execute("CREATE TABLE users(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, nome VARCHAR(255) NOT NULL, idade INT NOT NULL, email VARCHAR(255) NOT NULL)")
-
-
-
-# mycursor.execute("SELECT * FROM tabela_teste")
-
-# result = mycursor.fetchall()
-
-# for row in result:
-#   print(row)
